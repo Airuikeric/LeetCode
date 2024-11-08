eade1c0b356dadf95a3bca8b69d9f402d8ba1e3e
Airuikeric <118579075+Airuikeric@users.noreply.github.com>
2024-11-08 11:31:13 -0600

    Time: 0 ms (100.00%), Space: 43 MB (29.71%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -4,26 +4,17 @@ class Solution {
         if (nums == null || nums.length <= 0) {
             return 0;
         }
+
+        int left = 0; 
+        int right = nums.length - 1; 
         
-        /*
-             0, 1, 2, 3
-            [1, 3, 5, 6]
-            
-            target = 2
-            left = 2
-            right = 3
-                
-*/
-        int left = 0; // 0
-        int right = nums.length - 1; // 3
-        
-        while (left <= right) { // t, t
-            int mid = left + (right - left) / 2; // 1, 2
-            if (nums[mid] == target) { // f, f
+        while (left <= right) { // t ,t, t, f
+            int mid = left + (right - left) / 2; // 1, 2, 3
+            if (nums[mid] == target) { // f, f, f
                 return mid;
-            } else if (nums[mid] < target) { // t, f
+            } else if (nums[mid] < target) {  // t, t, t
                 left = mid + 1;
-            } else { // f
+            } else { 
                 right = mid - 1;
             }
         }
@@ -33,3 +24,11 @@ class Solution {
 }
 // time complexity: O(log n)
 // space complexity: O(1)
+
+/*
+    nums = [1, 3, 5, 6]  target = 7
+    index:  0, 1, 2, 3
+    left = 0, 2, 3, 4
+    right = 3
+    mid = 1, 2, 3
+*/
