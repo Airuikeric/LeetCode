c15b7c9f04f5b46508aea803d02165a40d09c2db
Airuikeric <118579075+Airuikeric@users.noreply.github.com>
2024-11-17 12:40:46 -0600

    Time: 2 ms (78.60%), Space: 53.4 MB (9.76%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,18 +1,23 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int majority = nums[0];
-        int votes = 1;
+        int count = 0;
+        int result = 0;
         
-        for (int i = 1; i < nums.length; i++) {
-            if (votes == 0) {
-                votes++;
-                majority = nums[i];
-            } else if (majority == nums[i]) {
-                votes++;
+        for (int num : nums) {
+            if (count == 0) {
+                result = num;
+            }
+            
+            if (num != result) {
+                count--;
             } else {
-                votes--;
+                count++;
             }
         }
-        return majority;
+        
+        return result;
     }
 }
+
+// time complexity: O(n)
+// space complexity: O(1)
