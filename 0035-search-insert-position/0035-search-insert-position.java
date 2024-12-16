c6bb1a548a6f4802c70018f731db7b8ebdb1f2b4
Airuikeric <118579075+Airuikeric@users.noreply.github.com>
2024-12-15 23:22:45 -0600

    Time: 0 ms (100.00%), Space: 42.5 MB (93.55%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,34 +1,20 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        // if the array is empty insert it into the array
-        if (nums == null || nums.length <= 0) {
-            return 0;
-        }
-
-        int left = 0; 
-        int right = nums.length - 1; 
         
-        while (left <= right) { // t ,t, t, f
-            int mid = left + (right - left) / 2; // 1, 2, 3
-            if (nums[mid] == target) { // f, f, f
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
                 return mid;
-            } else if (nums[mid] < target) {  // t, t, t
+            } else if (nums[mid] < target) {
                 left = mid + 1;
-            } else { 
+            } else {
                 right = mid - 1;
             }
         }
-        return left;
         
+        return left;
     }
 }
-// time complexity: O(log n)
-// space complexity: O(1)
-
-/*
-    nums = [1, 3, 5, 6]  target = 7
-    index:  0, 1, 2, 3
-    left = 0, 2, 3, 4
-    right = 3
-    mid = 1, 2, 3
-*/
