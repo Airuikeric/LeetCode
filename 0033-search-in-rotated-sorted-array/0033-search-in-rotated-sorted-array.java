5964e91fd146014b38d3cec6f8bfc435a5f55451
Airuikeric <118579075+Airuikeric@users.noreply.github.com>
2024-12-26 14:37:18 -0600

    Time: 0 ms (100.00%), Space: 42.1 MB (42.18%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,41 +1,51 @@
 class Solution {
     public int search(int[] nums, int target) {
-        if (nums == null || nums.length == 0) {
+        // base case
+         if (nums == null || nums.length <= 0) {
             return -1;
-        }
+         }
         
         int left = 0;
         int right = nums.length - 1;
         
+        // Step 1: Find the pivot point (smallest elemenmt in the array)
         while (left < right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] > nums[right]) {
+                // pivot is in the right half
                 left = mid + 1;
             } else {
+                // pivot is in the left half
                 right = mid;
             }
         }
         
+        // left is now at the pivot point
         int start = left;
+        
+        // reset search range
         left = 0;
         right = nums.length - 1;
         
+        // step 2: Decide the search range
         if (target >= nums[start] && target <= nums[right]) {
-            left = start;
+            left = start; // Target lies in the left sorted half
         } else {
-            right = start;
+            right = start - 1; // target lies in the left sorted half
         }
         
+        // step 3: perform binary search in the selected range
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (nums[mid] == target) {
-                return mid;    
+                return mid;
             } else if (nums[mid] < target) {
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
+        
         return -1;
     }
 }
