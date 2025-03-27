92e105b70210384757edc9025f31a2124e4b8f17
Airuikeric <118579075+Airuikeric@users.noreply.github.com>
2025-03-26 23:54:34 -0500

    Time: 482 ms (94.11%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -2,5 +2,8 @@
 SELECT w1.id
 FROM Weather w1
 JOIN Weather w2
+--	•	DATEDIFF(date1, date2) gives how many days between date1 and date2.
+--	•	So DATEDIFF(w1.recordDate, w2.recordDate) = 1 means w1 is 1 day after w2.
   ON DATEDIFF(w1.recordDate, w2.recordDate) = 1
 WHERE w1.temperature > w2.temperature;
+
